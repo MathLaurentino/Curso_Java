b6b94659be4ce47cc918044f2c0bcd13111e04e2
Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-02-26 17:14:02 -0300

    Add exercício - Set

---
+++ 9_Generics_Set_Map/Set/Exercicio1/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import entities.LogEntry;
+
+public class Program {
+    public static void main(String[] args) {
+
+    	try (BufferedReader br = new BufferedReader(new FileReader("doc.txt"))) {
+
+            Set<LogEntry> set = new HashSet<>();
+
+            String line = br.readLine();
+            while(line != null) {
+                String[] fields = line.split(" ");
+                String userName = fields[0];
+                Date moment = Date.from(Instant.parse(fields[1]));
+
+                set.add(new LogEntry(userName, moment));
+
+                line = br.readLine();
+            }
+
+            System.out.println("Total users: " + set.size());
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+    }
+
+}
